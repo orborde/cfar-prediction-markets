da6371942f184387037db63215b3f8fbade854e3
William Ehlhardt <williamehlhardt@gmail.com>
2017-06-11 00:56:02 -0700

    Include actual birth month distribution data.

--- birthmonthreal.py
+++ birthmonthreal.py
@@ -1,3 +1,4 @@
+import collections
 import csv
 import itertools
 import random
@@ -28,6 +29,31 @@ for e in data:
 
 assert len(LOOKUP) == 18
 
+def load_distribution():
+    dist = collections.defaultdict(int)
+    fn = open('bdata.txt')
+    header = fn.readline().strip()
+    print repr(header)
+    assert header == 'date count'
+    for line in fn:
+        line = line.strip()
+        date, count = line.split()
+        if date == 'total':
+            continue
+        count = int(count)
+        month_num = int(date[:2])
+        month = MONTHS[month_num - 1]
+        #print line, ':', month, count
+        dist[month] += count
+    return dict(dist)
+
+MONTH_DIST = load_distribution()
+MONTHS_EXPANDED = []
+for m, ct in MONTH_DIST.iteritems():
+    MONTHS_EXPANDED += [m] * ct
+print len(MONTHS_EXPANDED)
+assert len(MONTHS_EXPANDED) == sum(MONTH_DIST.values())
+
 def count_collisions(assignment):
     count = 0
     hits = 0
@@ -46,9 +72,13 @@ def generate_assignment():
     assignment = {}
     for name, month in LOOKUP.iteritems():
         if month == '':
-            month = random.choice(MONTHS)
+            month = random.choice(MONTHS_EXPANDED)
         assignment[name] = month
     return assignment
+
+for month in MONTHS:
+    print month, MONTH_DIST[month],
+    print MONTH_DIST[month]*100 / sum(MONTH_DIST.values())
 
 import tqdm
 count = 0
